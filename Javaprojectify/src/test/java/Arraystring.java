457a149cbce5095d622731c487297d40973b982d
ifyEze83 <drifyjo@yahoo.com>
2023-12-15 15:06:07 -0600

    Updates

---
+++ Javaprojectify/src/test/java/Arraystring.java
@@ -0,0 +1,22 @@
+package test.java;
+
+public class Arraystring {
+
+	public static void main(String[] args) {
+		String s[]=new String[4];
+		s[0]="Welcome";
+		s[1]="To";
+		s[2]="The";
+		s[3]="World";
+		
+		//System.out.println(s.length);//
+		
+		
+		for (String i:s)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
