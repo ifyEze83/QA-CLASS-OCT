fba231c914c13897dcf722dbe72a59b55031c755
ifyEze83 <drifyjo@yahoo.com>
2023-12-15 15:13:14 -0600

    Updates

---
+++ Javaprojectify/src/test/java/Arrayobject.java
@@ -0,0 +1,23 @@
+package test.java;
+
+public class Arrayobject {
+	public static void main(String[] args) {
+		
+	
+	Object a[]=new Object[5];
+	
+	a[0]=20;
+	a[1]=30.5;
+	a[2]="Helloworld";
+	a[3]="Gee";
+	a[4]=true;
+	
+	for(Object i:a)
+		
+	{
+		System.out.println(i);
+	}
+	
+	
+	}
+}
