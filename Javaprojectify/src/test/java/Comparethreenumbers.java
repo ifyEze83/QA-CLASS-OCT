f4742bc2f2c025d1038f85223cb5207372a5824a
ifyEze83 <drifyjo@yahoo.com>
2023-12-16 17:04:11 -0600

    update

---
+++ Javaprojectify/src/test/java/Comparethreenumbers.java
@@ -0,0 +1,25 @@
+package test.java;
+
+public class Comparethreenumbers {
+  
+	public static void main(String[]args) {
+		
+		int a=6;
+		int b=7;
+		int c=8;
+		
+		
+		if((a>b) && (a>c)) {
+			System.out.println("a is largest");
+			}
+		else if((b>a) && (b>c)) 
+		{
+			System.out.println("b is largest");
+		}
+		else {
+			System.out.println("c is largest");
+		}
+		
+
+}
+	}
